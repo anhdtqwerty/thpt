cd3ccfbf9242f03036c9c02096c8f41a7346c60b
Đào Tuấn Anh <anhdt.developer@gmail.com>
2020-12-30 23:34:50 +0700

    update

--- src/modules/factor/FactorEditDialog.vue
+++ src/modules/factor/FactorEditDialog.vue
@@ -36,7 +36,7 @@
   </v-dialog>
 </template>
 <script>
-import FactorInfoForm from '@/components/basic/form/FactorForm.vue'
+import FactorInfoForm from '@/components/basic/form/FactorNewForm.vue'
 import { mapActions, mapState } from 'vuex'
 
 export default {
