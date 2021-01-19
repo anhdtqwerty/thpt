e673d6a2356e501d29d0652e5a5d01555b972488
Nguyen Khanh <khanhnq.298@gmail.com>
2021-01-19 15:15:46 +0700

    fix menu ui

--- src/components/layout/AdminToolbar.vue
+++ src/components/layout/AdminToolbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-list class="pa-2" v-if="role.name === 'Manager'">
+  <v-list class="admin-toolbar" v-if="role.name === 'Manager'">
     <Dashboard />
     <Students />
     <Teachers />
@@ -26,10 +26,16 @@ export default {
     Teachers,
     Scores,
     AdvanceSetting,
-    System
+    System,
   },
   computed: {
-    ...mapState('auth', ['profile', 'role'])
-  }
+    ...mapState('auth', ['profile', 'role']),
+  },
 }
 </script>
+
+<style>
+.v-navigation-drawer__content .v-list-item__title {
+  font-size: 14px;
+}
+</style>
