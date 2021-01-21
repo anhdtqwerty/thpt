989e08385c5a29860bdc8252d4b3368e831fe27b
dangthevang <dangthevang2000@gmail.com>
2021-01-21 22:45:25 +0700

    add subject filter

---
+++ src/modules/subject/SubjectFilter.vue
@@ -0,0 +1,112 @@
+<template>
+  <v-form ref="form">
+    <v-row>
+      <v-col cols="12" md="10">
+        <v-row>
+          <v-col cols="12" md="4">
+            <autocomplete-subject
+              v-model="subject"
+              item-text="title"
+              clearable
+              clear-icon="mdi-close"
+              outlined
+              placeholder="Môn học"
+              item-value="id"
+              dense
+              deletable-chips
+              :hide-details="$vuetify.breakpoint.smAndDown"
+              :class="{ 'mb-4': $vuetify.breakpoint.smAndDown }"
+            />
+          </v-col>
+          <v-col cols="12" md="4">
+            <autocomplete-grade
+              v-model="grade"
+              item-text="name"
+              clearable
+              clear-icon="mdi-close"
+              outlined
+              placeholder="Khối"
+              item-value="id"
+              dense
+              deletable-chips
+              :hide-details="$vuetify.breakpoint.smAndDown"
+              :class="{ 'mb-4': $vuetify.breakpoint.smAndDown }"
+            />
+          </v-col>
+          <v-col cols="12" md="4">
+            <autocomplete-division
+              v-model="divisions"
+              item-text="name"
+              item-value="id"
+              clear-icon="mdi-close"
+              clearable
+              placeholder="Phân ban"
+              outlined
+              dense
+              large
+              deletable-chips
+              :hide-details="$vuetify.breakpoint.smAndDown"
+              :class="{ 'mb-4': $vuetify.breakpoint.smAndDown }"
+            />
+          </v-col>
+        </v-row>
+      </v-col>
+
+      <v-col cols="12" md="2">
+        <v-btn
+          height="40"
+          dark
+          @click="onFilterChanged()"
+          color="primary"
+          outlined
+        >
+          <v-icon left dark>mdi-filter-outline</v-icon>Lọc
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-form>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import moment from 'moment'
+import AutocompleteSubject from '@/components/basic/input/AutocompleteSubject.vue'
+import AutocompleteDivision from '@/components/basic/input/AutocompleteDivision.vue'
+import AutocompleteGrade from '@/components/basic/input/AutocompleteGrade.vue'
+
+export default {
+  components: {
+    AutocompleteSubject,
+    AutocompleteDivision,
+    AutocompleteGrade,
+  },
+  data: () => ({
+    subject: '',
+    query: '',
+    division:'',
+    grade:'',
+    dialog: true,
+  }),
+  computed: {
+    ...mapState('constant', ['subjectsStatus']),
+  },
+  methods: {
+    onFilterChanged() {
+    this.$emit('onFilterChanged', {
+        grade: this.grade,
+        divisions: this.divisions,
+        subject: this.subject.Title,
+      }),
+    this.reset()
+    },
+    reset() {
+      this.query = ''
+      this.grade = ''
+      this.divisions = ''
+      this.subject = ''
+    }
+  },
+}
+</script>
+
+<style scoped></style>
